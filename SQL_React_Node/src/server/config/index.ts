16dc9b189814ed303953f86b8b11592f38ec42dc
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2022-01-12 18:25:59 +0100

    config deleted from git ignore

---
+++ SQL_React_Node/src/server/config/index.ts
@@ -0,0 +1,11 @@
+export default {
+
+    mysql: {
+        host: 'localhost',
+            port: 3306,
+            user: 'user1',
+            database: 'all_data',
+            password: 'password'
+    }
+
+}
