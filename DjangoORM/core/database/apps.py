0a718548c27124c38803275db6d912aec4a83f38
panjacob <s19615@pjwstk.edu.pl>
2022-01-17 13:37:22 +0100

    Init data

--- DjangoORM/core/database/apps.py
+++ DjangoORM/core/database/apps.py
@@ -1,6 +1,22 @@
+import json
+
 from django.apps import AppConfig
 
 
 class DatabaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'database'
+
+    def ready(self):
+        import os
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            init_data()
+
+
+def load_data():
+    with open('data.json', encoding='utf-8') as json_file:
+        return json.load(json_file)
+
+
+def init_data():
+    from DjangoORM.core.database.models import models
