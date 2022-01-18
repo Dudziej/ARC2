273f48c11c519a2660be72eed78b0163947e4fa3
Dudziej <dudzik32@gmail.com>
2022-01-18 20:41:36 +0100

    New Grid and place for requests

--- SQL_React_Node/src/client/App.tsx
+++ SQL_React_Node/src/client/App.tsx
@@ -30,129 +30,282 @@ const App = (props: AppProps) => {
 
 	return (
 		<main className="container my-5">
-			<h1 className="text-primary text-center">SQL vs NOSQL vs SQL + ORM</h1>
-			{/* instrukcja jak wstawiac divy + col-sm-5 do zmiany jak dojdzie SQLite
-				<div>
-					MySQL
+			<h1 className="text-primary text-center">Benchmark Baz Danych!</h1>
+			<div className="row justify-content-center mt-4 mb-4">
+				<div className="col-sm-4 border rounded shadow text-primary text-center">
+					<h3 className="mt-2 mb-2">Specyfikacja Komputera</h3>
+						<p>
+							Procesor - Intel Core i7-8700 4.60GHz
+						</p>
+						<p>
+							Pamięć - Samsung 500GB M.2 PCIe NVMe 970 EVO Plus
+						</p>
+						<p>
+							Pamięć(Ram) - DDR4 16GB 2666 MHz
+						</p>
+						<p>
+							System - Windows 10 64 bit
+						</p>
 				</div>
-
-				<div>
-					MongoDB
-				</div>
-
-				<div>
-					SQLite
-				</div>
-
-				...REPEAT
-			*/}
+			</div>
+			<h2 className="text-primary text-center">SQL vs NOSQL vs SQL + ORM</h2>
 			<div className='row justify-content-center'>
-				<div className='col-sm-5 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+				{/*-------------------1-1-1---------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Cała tabela "Zegarek"</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p>SELECT * from data</p>
 					<GetAll/>
 				</div>
 
-				<div className='col-sm-5 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p>find()</p>
 					<MongoGet1_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*----------------2-1-1----------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Zegarki gdzie wzór == “logo”</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p>SELECT * from data WHERE Rodzaj = 'Analogowe'</p>
 					<GetAnalogowe/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet2_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>SELECT * from data WHERE Wodoszczelność = '200m'</p>
-					<GetWodoszczelne200mAll/>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*------------------2-1-2------------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet2_1_2/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>SELECT * from data WHERE Producent = 'Lorus  Zegarki Lorus'</p>
-					<GetProducentLorus/>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*----------------2-2-1------------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>find(Producent: 'Lorus Zegarki Lorus')</p>
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
 					<MongoGet2_2_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>SELECT * from data WHERE LENGTH(Wzór)  0</p>
-					<GetKrajProducentaIstnieje/>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*------------------2-3-1------------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet2_3_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>SELECT * from data WHERE MOD(id,2) = 0</p>
-					<IdParzyste/>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*----------------3-1-1---------------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Wszystkie wzory zegarków</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p>SELECT * from data WHERE Wodoszczelność = '200m'</p>
+					<GetWodoszczelne200mAll/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet3_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*-------------------3-2-1------------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet3_2_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*--------------------4-1-1-----------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Zegarki gdzie wzór == “logo” i kolor == “czarny” OR kolor == ‘czarne’</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p>SELECT * from data WHERE Producent = 'Lorus  Zegarki Lorus'</p>
+					<GetProducentLorus/>
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet4_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*---------------------4-2-1----------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet4_2_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*-------------------5-1-1------------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Zegarki gdzie wzór == “logo” i  kolor != “czarny”</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p>SELECT * from data WHERE LENGTH(Wzór)  0</p>
+					<GetKrajProducentaIstnieje/>
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet5_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*--------------------5-2-1-----------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet5_2_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*------------------6-1-1-------------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Wszystkie zegarki posortowane alfabetycznie po kolorze</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p>SELECT * from data WHERE MOD(id,2) = 0</p>
+					<IdParzyste/>
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet6_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*------------------6-2-1-------------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet6_2_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*------------------7-1-1-------------------------------*/}
+				<hr className="mt-4"/>
+				<h3 className="text-primary text-center">Wszystkie kolory zaczynające się na “cz”</h3>
+				<hr className="mb-4"/>
+
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet7_1_1/>
 				</div>
 
-				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				{/*------------------7-2-1-------------------------------*/}
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+
+				<div className='col-sm-4 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p></p>
 					<MongoGet7_2_1/>
 				</div>
-			</div>
 
+				<div className='col-sm-4 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+
+				</div>
+				{/*------------------------------------------------------*/}
+			</div>
 		</main>
 	);
 };
