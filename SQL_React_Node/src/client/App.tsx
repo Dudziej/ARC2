8cc56ae84a3b5113ec874ab0b37341c9c13c6f83
Dudziej <dudzik32@gmail.com>
2022-01-18 19:58:56 +0100

    React manual for Kuba + new div's for mongo // MongoDB

--- SQL_React_Node/src/client/App.tsx
+++ SQL_React_Node/src/client/App.tsx
@@ -7,6 +7,20 @@ import GetProducentLorus from "./Components/SQL/GetProducentLorus";
 import GetKrajProducentaIstnieje from "./Components/SQL/GetKrajProducentaIstnieje";
 import IdParzyste from "./Components/SQL/IdParzyste";
 import MongoGet1_1_1 from "./Components/NoSQL/MongoGet1_1_1";
+import MongoGet2_1_1 from "./Components/NoSQL/MongoGet2_1_1";
+import MongoGet2_1_2 from "./Components/NoSQL/MongoGet2_1_2";
+import MongoGet2_2_1 from "./Components/NoSQL/MongoGet2_2_1";
+import MongoGet2_3_1 from "./Components/NoSQL/MongoGet2_3_1";
+import MongoGet3_1_1 from "./Components/NoSQL/MongoGet3_1_1";
+import MongoGet3_2_1 from "./Components/NoSQL/MongoGet3_2_1";
+import MongoGet4_1_1 from "./Components/NoSQL/MongoGet4_1_1";
+import MongoGet4_2_1 from "./Components/NoSQL/MongoGet4_2_1";
+import MongoGet5_1_1 from "./Components/NoSQL/MongoGet5_1_1";
+import MongoGet5_2_1 from "./Components/NoSQL/MongoGet5_2_1";
+import MongoGet6_1_1 from "./Components/NoSQL/MongoGet6_1_1";
+import MongoGet6_2_1 from "./Components/NoSQL/MongoGet6_2_1";
+import MongoGet7_1_1 from "./Components/NoSQL/MongoGet7_1_1";
+import MongoGet7_2_1 from "./Components/NoSQL/MongoGet7_2_1";
 
 const App = (props: AppProps) => {
 
@@ -17,7 +31,21 @@ const App = (props: AppProps) => {
 	return (
 		<main className="container my-5">
 			<h1 className="text-primary text-center">SQL vs NOSQL vs SQL + ORM</h1>
+			{/* instrukcja jak wstawiac divy + col-sm-5 do zmiany jak dojdzie SQLite
+				<div>
+					MySQL
+				</div>
 
+				<div>
+					MongoDB
+				</div>
+
+				<div>
+					SQLite
+				</div>
+
+				...REPEAT
+			*/}
 			<div className='row justify-content-center'>
 				<div className='col-sm-5 text-center card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p>SELECT * from data</p>
@@ -35,8 +63,8 @@ const App = (props: AppProps) => {
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>find(Rodzaj: 'Analogowe')</p>
-
+					<p></p>
+					<MongoGet2_1_1/>
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
@@ -45,8 +73,8 @@ const App = (props: AppProps) => {
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>find(Wodoszczelność: '200m')</p>
-
+					<p></p>
+					<MongoGet2_1_2/>
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
@@ -56,7 +84,7 @@ const App = (props: AppProps) => {
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
 					<p>find(Producent: 'Lorus Zegarki Lorus')</p>
-
+					<MongoGet2_2_1/>
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
@@ -65,8 +93,8 @@ const App = (props: AppProps) => {
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>find(Wzór: $exists: true)</p>
-
+					<p></p>
+					<MongoGet2_3_1/>
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
@@ -75,8 +103,53 @@ const App = (props: AppProps) => {
 				</div>
 
 				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
-					<p>find(id: $mod: [2, 0])</p>
+					<p></p>
+					<MongoGet3_1_1/>
+				</div>
 
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet3_2_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet4_1_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet4_2_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet5_1_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet5_2_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet6_1_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet6_2_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet7_1_1/>
+				</div>
+
+				<div className='col-sm-5 text-center border card shadow mt-1 mb-1 pt-2 pb-2'>
+					<p></p>
+					<MongoGet7_2_1/>
 				</div>
 			</div>
 
