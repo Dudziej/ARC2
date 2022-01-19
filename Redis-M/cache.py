3d381d8244e1156358388896b404738f26a88120
Dudziej <dudzik32@gmail.com>
2022-01-19 13:42:47 +0100

    data change

--- Redis-M/cache.py
+++ Redis-M/cache.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-with open('../Scrapper/data.json') as json_file:
+with open('../Scrapper/data_faker.json') as json_file:
     data = json.load(json_file)
     for obj in data:
         jako_string = json.dumps(obj)
