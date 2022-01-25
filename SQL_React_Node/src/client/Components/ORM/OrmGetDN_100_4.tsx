38b616ac5c173b05c455b31ac0278186e3ed051a
Dudziej <dudzik32@gmail.com>
2022-01-25 22:04:13 +0100

    DN_100_4 frontend update //ORM

--- SQL_React_Node/src/client/Components/ORM/OrmGetDN_100_4.tsx
+++ SQL_React_Node/src/client/Components/ORM/OrmGetDN_100_4.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {useState, useEffect} from 'react';
 
-const OrmGet6_1_1 = (props: AppProps) => {
+const OrmGetDN_100_4 = (props: AppProps) => {
 
     const [exTime, setExTime] = useState(0);
 
     async function getData() {
         try {
             let start = Date.now();
-            const res = await fetch('http://127.0.0.1:8000/6_1_1');
+            const res = await fetch('http://127.0.0.1:8000/DN_100_4');
             const data = await res.status===200;
             let end = Date.now();
 
@@ -33,4 +33,4 @@ const OrmGet6_1_1 = (props: AppProps) => {
 interface AppProps {
 }
 
-export default OrmGet6_1_1;
+export default OrmGetDN_100_4;
